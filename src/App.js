9840fda73407c7b8d17ec47285e81773c12e7672
anubhav bisht <anubhavbisht98@gmail.com>
2024-03-28 17:08:44 +0530

    configured tailwind css for project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import "../index.css";
+import "../index-root.css";
 import RestaurantMenu from "./components/RestaurantMenu";
 import OfflineOnlinePopup from "./components/OfflineOnlinePopup";
 import LoadingPage from "./components/LoadingPage";

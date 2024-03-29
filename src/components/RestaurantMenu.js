11c930974689687fcade8304ebe780fdb0595ac0
anubhav bisht <anubhavbisht98@gmail.com>
2024-03-29 09:23:24 +0530

    some more ui changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -3,26 +3,39 @@ import { useParams } from "react-router-dom";
 import { useRestaurantMenu } from "../utils/customHooks";
 
 const RestaurantMenu = () => {
-  const params = useParams();
-  const restaurantId = params.resId;
-  const resInfo = useRestaurantMenu(restaurantId);
-  if (resInfo === null) return <Shimmer />;
-  const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[2]?.card?.card?.info;
-  const { itemCards } =
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  const { resId } = useParams();
+  const resInfo = useRestaurantMenu(resId);
+
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
+  const {
+    name,
+    cuisines: cuisineNames = [],
+    costForTwoMessage = "",
+  } = resInfo.cards[2]?.card?.card?.info;
+  const itemCards =
+    resInfo.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
+      ?.itemCards;
+
   return (
-    <div className="menu">
-      <h1>{name}</h1>
-      <h3>
-        {cuisines.join(", ")} - {costForTwoMessage}
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold">{name}</h1>
+      <h3 className="text-lg text-gray-500">
+        {cuisineNames.join(", ")} - {costForTwoMessage}
       </h3>
-      <h3></h3>
-      <ul>
+      <h3 className="text-xl font-medium mt-4">Menu</h3>
+      <ul className="list-none mt-4">
         {itemCards?.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} - Rs
-            {(item.card.info.price || item.card.info.defaultPrice) / 100}
+          <li
+            key={item.card.info.id}
+            className="flex justify-between border-b border-gray-200 py-2"
+          >
+            <span className="text-base font-medium">{item.card.info.name}</span>
+            <span className="text-base font-medium">
+              Rs {(item.card.info.price || item.card.info.defaultPrice) / 100}
+            </span>
           </li>
         ))}
       </ul>
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,7 +1,7 @@
 const Shimmer = () => {
   return (
     <div className="flex flex-wrap justify-between mt-5">
-      {[...Array(18)].map((_, index) => (
+      {[...Array(20)].map((_, index) => (
         <div key={index} className="shimmer-card"></div>
       ))}
     </div>
